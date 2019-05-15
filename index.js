575330a6c2b5d1bdcadf52203182e1abb196eaf3
Yoonmi Kim <yoonmi.kim@rutgers.edu>
2019-05-15 14:16:05 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Write your solution here!
+// const arr = ["Milo", "Otis", "Garfield"];
+
+// arr.append("Odie");
+// arr.prepend("Odie");
+// arr.removeLast();
+// arr.removeFirst();
+
+const append = ["Milo", "Otis", "Garfield"];
+const prepend = ["Milo", "Otis", "Garfield"];
+const removeLast = ["Milo", "Otis", "Garfield"];
+const removeFirst = ["Milo", "Otis", "Garfield"];
+
+append.push("Odie");
+prepend.unshift("Odie");
+removeLast.pop();
+removeFirst.shift();
